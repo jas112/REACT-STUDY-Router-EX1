40728eb85db407ee2b64bfb9d8c87ea898eec69d
jas112 <jas.sandmaster@gmail.com>
2022-08-13 23:04:34 -0700

    modifications made on VendingMachine, Chips, and Sodas component/pages.

--- src/components/chips/Chips.js
+++ src/components/chips/Chips.js
@@ -1,6 +1,46 @@
 import React, { Component } from 'react';
 
 class Chips extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            chips: JSON.parse(window.localStorage.getItem('chips') || '[]'),
+            chipCount: JSON.parse(window.localStorage.getItem('chipCount') || '0'),
+        }
+        this.getBagOfChips = this.getBagOfChips.bind(this);
+    }
+
+    generateOrientation(){
+
+        let rotationAngle = Math.random() * 90 - 45;
+        let xPosition = Math.random() * 40 - 20;
+        let yPosition = Math.random() * 40 - 20;
+
+        let orientation = `translate(${xPosition}px, ${yPosition}px) rotate(${rotationAngle}deg)`;
+
+        return orientation;
+    }
+
+    getBagOfChips(){
+        let newChipSelection = {};
+        let chipTransform = this.generateOrientation();
+        newChipSelection['orientation'] = chipTransform;
+        let newChips = [...this.state.chips, newChipSelection];
+
+        this.setState({chips: newChips, chipCount: this.state.chipCount+=1}, () => {
+            console.log(`Udating local storage...`);
+            window.localStorage.setItem('chips', JSON.stringify(this.state.chips));
+            window.localStorage.setItem('chipCount', JSON.stringify(this.state.chipCount));
+        });
+    }
+
+    resetChips(){
+        window.localStorage.setItem('chips', '[]');
+        window.localStorage.setItem('chipCount', '0');
+
+        this.setState({jokes: [], currJoke: {}});
+    }
+
   render() {
     return (
         <div>
--- src/components/soda/Soda.js
+++ src/components/soda/Soda.js
@@ -1,6 +1,46 @@
 import React, { Component } from 'react';
 
 class Soda extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            sodas: JSON.parse(window.localStorage.getItem('sodas') || '[]'),
+            sodaCount: JSON.parse(window.localStorage.getItem('sodaCount') || '0'),
+        }
+        this.getASoda = this.getASoda.bind(this);
+    }
+
+    generateOrientation(){
+
+        let rotationAngle = Math.random() * 90 - 45;
+        let xPosition = Math.random() * 40 - 20;
+        let yPosition = Math.random() * 40 - 20;
+
+        let orientation = `translate(${xPosition}px, ${yPosition}px) rotate(${rotationAngle}deg)`;
+
+        return orientation;
+    }
+
+    getASoda(){
+        let newSodaSelection = {};
+        let sodaTransform = this.generateOrientation();
+        newSodaSelection['orientation'] = sodaTransform;
+        let newSodas = [...this.state.sodas, newSodaSelection];
+
+        this.setState({sodas: newSodas, sodaCount: this.state.sodaCount+=1}, () => {
+            console.log(`Updating local storage...`);
+            window.localStorage.setItem('sodas', JSON.stringify(this.state.sodas));
+            window.localStorage.setItem('sodaCount', JSON.stringify(this.state.sodaCount));
+        });
+    }
+
+    resetSodas(){
+        window.localStorage.setItem('sodas', '[]');
+        window.localStorage.setItem('sodaCount', '0');
+
+        this.setState({sodas: [], sodaCount: 0});
+    }
+
   render() {
     return (
         <div>
