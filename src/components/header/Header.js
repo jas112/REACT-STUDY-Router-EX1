f49e7199e48573c0e7c218caf9315d1164a4d5f3
jas112 <jas.sandmaster@gmail.com>
2022-08-14 17:48:41 -0700

    react router navlink imlemented in Header component.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import './Header.css';
 class Header extends Component {
   render() {
@@ -15,10 +15,11 @@ class Header extends Component {
         {/* </div> */}
         <div className='Header-console'>
           {/* React Router Exercise B Begin */}
-          <Link to='/' className='Header-navLink'>V-MACHINE</Link><br/>
-          <Link to='/chips' className='Header-navLink'>CHIPS</Link>
-          <Link to='/sardines' className='Header-navLink'>SARDINES</Link>
-          <Link to='/soda' className='Header-navLink'>SODA</Link>
+          {/* <NavLink to='/' className={!isActive ? 'Header-navLink' : 'Header-navLink-active'}>V-MACHINE</NavLink><br/> */}
+          <NavLink to='/' className={({ isActive }) => (isActive ? 'Header-navLink-active' : 'Header-navLink')}>V-MACHINE</NavLink><br/>
+          <NavLink to='/chips' className={({ isActive }) => (isActive ? 'Header-navLink-active' : 'Header-navLink')}>CHIPS</NavLink>
+          <NavLink to='/sardines' className={({ isActive }) => (isActive ? 'Header-navLink-active' : 'Header-navLink')}>SARDINES</NavLink>
+          <NavLink to='/soda' className={({ isActive }) => (isActive ? 'Header-navLink-active' : 'Header-navLink')}>SODA</NavLink>
           {/* React Router Exercise B End */}
         </div>
       </div>
